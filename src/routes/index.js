c5ebb7487158c54b84f737436d2ce77fd319ded0
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-26 20:57:26 +0300

    Up

---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+"use strict";
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/:
+
+// URL: /
+
+// auth:
+router.use("/auth", require("./authRouter"));
+// user:
+router.use("/users", require("./userRouter"));
+// token:
+router.use("/tokens", require("./tokenRouter"));
+
+// blog:
+router.use("/blogs", require("./blogRouter"));
+// category:
+router.use("/categories", require("./categoryRouter"));
+
+// comment:
+router.use("/comments", require("./commentRouter"));
+
+// document:
+router.use("/documents", require("./document"));
+
+/* ------------------------------------------------------- */
+module.exports = router;
