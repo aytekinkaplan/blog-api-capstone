24bd6c114189cb8a97dd3a24fee0c471007f0e2a
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-30 00:53:46 +0300

    Up

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,14 @@ const express = require("express");
 const router = express.Router();
 const Blog = require("../models/blogModel");
 const Category = require("../models/categoryModel");
-const Comment = require("../models/commentModel"); // Modeli içe aktarma
+const Comment = require("../models/commentModel");
 
-// Root path to render the home page:
+// Root path to render the home page
 router.get("/", async (req, res) => {
   try {
     const blogs = await Blog.find().populate("categoryId");
     const categories = await Category.find();
-    const comments = await Comment.find().populate("blogId"); // Modeli kullanma
+    const comments = await Comment.find().populate("blogId");
 
     res.render("index", { blogs, categories, comments });
   } catch (error) {
@@ -22,30 +22,61 @@ router.get("/", async (req, res) => {
   }
 });
 
+// About page
 router.get("/about", (req, res) => {
-  res.render("about"); // 'about' EJS dosyasını render eder
+  res.render("about");
 });
+
+// Contact page
 router.get("/contact", (req, res) => {
-  res.render("contact"); // 'about' EJS dosyasını render eder
+  res.render("contact");
 });
-// URL: /
 
+// Blog post detail page
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id).populate("categoryId");
+    const comments = await Comment.find({ blogId: req.params.id }).populate(
+      "userId"
+    );
+
+    if (!blog) {
+      return res.status(404).render("404", { message: "Blog post not found" });
+    }
+
+    res.render("post", { blog, comments });
+  } catch (error) {
+    console.error("Error fetching blog details:", error);
+    res.status(500).json({ error: "Error fetching blog details" });
+  }
+});
+
+// Like a blog post
+router.post("/posts/:id/like", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      return res.status(404).json({ error: "Blog post not found" });
+    }
+
+    blog.likes += 1;
+    await blog.save();
+
+    res.status(200).json({ likes: blog.likes });
+  } catch (error) {
+    console.error("Error updating likes:", error);
+    res.status(500).json({ error: "Error updating likes" });
+  }
+});
+
+// Routes for other resources
 router.use("/auth", require("./authRouter"));
-// user:
 router.use("/users", require("./userRouter"));
-// token:
 router.use("/tokens", require("./tokenRouter"));
-
-// blog:
 router.use("/blogs", require("./blogRouter"));
-// category:
 router.use("/categories", require("./categoryRouter"));
-
-// comment:
 router.use("/comments", require("./commentRouter"));
-
-// document:
 router.use("/documents", require("./document"));
 
-/* ------------------------------------------------------- */
 module.exports = router;
